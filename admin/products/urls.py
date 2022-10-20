15d6f4510328bc0ae20792353a97f44193ddd896
Ahmed Hamza Aydin <ahamzaaydin.py@gmail.com>
2022-10-20 16:45:07 +0300

    Initial commit

---
+++ admin/products/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from .views import ProductViewset, UserAPIView
+
+urlpatterns = [
+    path('products/', ProductViewset.as_view({
+        'get': 'list',
+        'post': 'create'
+    })),
+    path('products/<str:pk>/', ProductViewset.as_view({
+        'get': 'retrieve',
+        'put': 'update',
+        'delete': 'destroy'
+    })),
+    
+    path('users/', UserAPIView.as_view())
+]
